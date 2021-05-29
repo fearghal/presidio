a39510179e2b1a8fdb283793e7cf6253c9bfe6f0
fearghal <fearghal.sheehan@gmail.com>
2021-05-29 16:29:05 +0100

    Fix docstring flake8 error in IE PPSN recognizer code.

--- presidio-analyzer/presidio_analyzer/predefined_recognizers/ie_ppsn_recognizer.py
+++ presidio-analyzer/presidio_analyzer/predefined_recognizers/ie_ppsn_recognizer.py
@@ -4,7 +4,8 @@ from presidio_analyzer import Pattern, PatternRecognizer
 class IePpsnRecognizer(PatternRecognizer):
     """
     Recognizes Irish PPS number.
-    This is the Irish government's "personal public service number".
+
+    The PPS number is the Irish government's "personal public service number".
     A PPS Number is always 7 numbers followed by either one or two letters.
     https://www.gov.ie/en/service/12e6de-get-a-personal-public-service-pps-number
     """
